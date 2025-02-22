d663d055319a93d34ca56aad4bd38e6c6747f067
KalypsoExists <KalypsoExists@gmail.com>
2025-02-22 18:51:01 +0530

    Update

--- jgltf-model/src/main/java/de/javagl/jgltf/model/structure/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * Classes for handling the structure of accessors, buffer views, and buffers
- * in glTF.<br>
- * <br>
- * In most cases, these classes should not directly be used by clients.
- */
-package de.javagl.jgltf.model.structure;
-
